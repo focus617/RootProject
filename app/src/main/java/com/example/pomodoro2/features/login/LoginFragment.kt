e8935bd5023981ff16df7cd17c747fdafd658049
Xu Zhiyong <13980668829@163.com>
2020-08-03 20:05:26 +0800

    Introduce login feature.

---
+++ app/src/main/java/com/example/pomodoro2/features/login/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.example.pomodoro2.features.login
+
+import com.example.pomodoro2.R
+import com.example.pomodoro2.core.platform.BaseFragment
+
+class LoginFragment : BaseFragment() {
+    override fun layoutId(): Int {
+        return R.layout.fragment_login
+    }
+}
