3ea83b28bf05ac1755d43bbfd8cdafae21e3e3cc
Xu Zhiyong <13980668829@163.com>
2020-08-03 15:39:36 +0800

    create core.platform package with base class.

---
+++ app/src/main/java/com/example/pomodoro2/core/platform/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.pomodoro2.core.platform
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * Base Fragment class with helper methods for handling views and back button events.
+ *
+ * @see Fragment
+ */
+abstract class BaseFragment : Fragment() {
+
+    abstract fun layoutId(): Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+        inflater.inflate(layoutId(), container, false)
+
+    open fun onBackPressed() {}
+
+    internal fun firstTimeCreated(savedInstanceState: Bundle?) = savedInstanceState == null
+}
