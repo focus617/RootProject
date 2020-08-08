527207022d47b191944c413602bc6dbda4236561
Xu Zhiyong <13980668829@163.com>
2020-08-08 15:24:06 +0800

    Update SingleLiveEvent with detail sample.

--- app/src/main/java/com/example/pomodoro2/core/platform/SingleLiveEvent.kt
+++ app/src/main/java/com/example/pomodoro2/core/platform/SingleLiveEvent.kt
@@ -1,24 +1,29 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.pomodoro2.core.platform
 
 import androidx.lifecycle.Observer
 
 /**
- * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ * Define a custom Event class to make LiveData as a event wrapper which can be consumed only once
+ *
+ * @sample
+ * In ViewModel: showSnackBar() is exposed to trigger this one-time event
+ *     private val _showSnackBarEvent = MutableLiveData<SingleLiveEvent<Unit>>()
+ *     val showSnackBarEvent: LiveData<SingleLiveEvent<Unit>> = _showSnackBarEvent
+ *
+ *     private fun showSnackBar() {
+ *        _showSnackBarEvent.value = SingleLiveEvent(Unit)
+ *     }
+ *
+ * @sample
+ * In Fragment: Add an Observer on the state variable for showing a SnackBar message
+ *     viewModel.showSnackBarEvent.observe(viewLifecycleOwner, EventObserver {
+ *         Snackbar.make(
+ *            requireActivity().findViewById(android.R.id.content),
+ *            getString(R.string.goodbye_message),
+ *            Snackbar.LENGTH_SHORT // How long to display the message.
+ *         ).show()
+ *     })
+ *
  */
 open class SingleLiveEvent<out T>(private val content: T) {
 
