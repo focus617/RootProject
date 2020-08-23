3b2374782258e05b7999b469489c95c57a328daa
Xu Zhiyong <13980668829@163.com>
2020-08-23 23:38:53 +0800

    First demo OK: strategy pattern.

--- core/src/main/java/com/example/pomodoro2/designpattern/strategy/strategy.kt
+++ core/src/main/java/com/example/pomodoro2/designpattern/strategy/strategy.kt
@@ -1,13 +1,15 @@
 package com.example.pomodoro2.designpattern.strategy
 
+import com.example.pomodoro2.designpattern.utils.unwrapCompanionClass
+
 // 抽象方法类
 abstract class Strategy {
     // 算法方法
-    abstract fun algorithmInterface()
+    abstract fun algorithmInterface(context: Context)
 }
 
 // 抽象Context类
-abstract class StrategyContext {
+abstract class Context {
 
     private lateinit var _strategy: Strategy
 
@@ -17,7 +19,12 @@ abstract class StrategyContext {
 
     // 上下文接口
     fun contextInterface(){
-        _strategy.algorithmInterface()
+        _strategy.algorithmInterface(this)
+    }
+
+    // 对 Strategy 提供的接口
+    fun getInfo(){
+        println("Context: Hi,${unwrapCompanionClass(_strategy.javaClass).name}. 你想知道什么？")
     }
 }
 
@@ -25,28 +32,35 @@ abstract class StrategyContext {
 
 // 具体算法A
 class ConcreteStrategyA: Strategy(){
-    override fun algorithmInterface() {
-        println("算法A实现")
+    override fun algorithmInterface(context: Context) {
+        // 向Context请求数据
+        context.getInfo()
+        println("算法A实现: Say Hello!\n")
     }
 }
 
 // 具体算法B
 class ConcreteStrategyB: Strategy(){
-    override fun algorithmInterface() {
-        println("算法B实现")
+    override fun algorithmInterface(context: Context) {
+        // 向Context请求数据
+        context.getInfo()
+        println("算法B实现: Say Hello!\n")
     }
 }
 
 // 具体算法C
 class ConcreteStrategyC: Strategy(){
-    override fun algorithmInterface() {
-        println("算法C实现")
+    override fun algorithmInterface(context: Context) {
+        // 向Context请求数据
+        context.getInfo()
+        println("算法C实现: Say Hello!\n")
     }
 }
 
-class Context(algorithmType: String): StrategyContext(){
+class ConcreteContext(algorithmType: String): Context(){
     init{
         when(algorithmType){
+            // 同时应用了简单工厂模式
             "A" -> setStrategy(ConcreteStrategyA())
             "B" -> setStrategy(ConcreteStrategyB())
             "C" -> setStrategy(ConcreteStrategyC())
@@ -58,13 +72,15 @@ class Context(algorithmType: String): StrategyContext(){
 
 
 fun main() {
-    var context = Context("A")
+    var context: ConcreteContext?
+
+    context = ConcreteContext("A")
     context.contextInterface()
 
-    context = Context("B")
+    context = ConcreteContext("B")
     context.contextInterface()
 
-    context = Context("C")
+    context = ConcreteContext("C")
     context.contextInterface()
 }
 
---
+++ core/src/main/java/com/example/pomodoro2/designpattern/utils/Utils.kt
@@ -0,0 +1,15 @@
+package com.example.pomodoro2.designpattern.utils
+
+import kotlin.reflect.KClass
+
+// unwrap companion class to enclosing class given a Java Class
+fun <T : Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
+    return ofClass.enclosingClass?.takeIf {
+        ofClass.enclosingClass.kotlin.objectInstance?.javaClass == ofClass
+    } ?: ofClass
+}
+
+// unwrap companion class to enclosing class given a Kotlin Class
+fun <T: Any> unwrapCompanionClass(ofClass: KClass<T>): KClass<*> {
+    return unwrapCompanionClass(ofClass.java).kotlin
+}
