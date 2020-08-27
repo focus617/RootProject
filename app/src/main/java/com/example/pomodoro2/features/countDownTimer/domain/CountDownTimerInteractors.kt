28ac321b351db6fd331d9561ed2caab6659c7714
Xu Zhiyong <13980668829@163.com>
2020-08-27 09:18:44 +0800

    Improve use case of CountdownTimer.

--- app/src/main/java/com/example/pomodoro2/features/countDownTimer/domain/CountDownTimerInteractors.kt
+++ app/src/main/java/com/example/pomodoro2/features/countDownTimer/domain/CountDownTimerInteractors.kt
@@ -1,20 +1,30 @@
 package com.example.pomodoro2.features.countDownTimer.domain
 
 /**
- * Use Case of Activity
- * 1. Adding a activity to a currently selected task.
- * 2. Removing a activity from a currently selected task.
- * 3. Getting all activities for currently selected tasks.
- * 4. Getting selected activity
- * 5. Set selected activity
+ * Use Case of CountdownTimer
+ * 0.1 (Customer) click the right icon of selected subTask
+ * 0.2 (System) Launch countDownTimer fragment
+ *
+ * 1. (System) Launch Timer with tick-counting from selected subTask.
+ * 2. (Customer) Pause the Timer
+ *  2.1  (Customer) Resume the Timer
+ *  2.2 Timer continue
+ * 3.  (Customer) Reset the Timer
+ *  3.1 Timer restart with original tick-counting)
+ * 4. (System) Show dialog when Timer time-up, and play music at the same time to alert Customer
+ * 5. (Customer) Close the dialog
+ * 6. (System) Go back to task fragment
  */
 data class CountDownTimerInteractors(
     val tmp:Int = 0
     //TODO: Add use case set for countdown timer feature here
-/*    val addActivity: AddActivity,
-    val getActivities: GetActivities,
-    val deleteActivity: RemoveActivity,
-    val getSelectedActivity: GetSelectedActivity,
-    val setSelectedActivity: SetSelectedActivity
-*/
+/*
+    val launchTimerUseCase
+    val pauseTimerUseCase
+    val resumeTimerUseCase
+    val resetTimerUseCase
+    val TimeupCallback??
+ */
+
+
 )
--- app/src/main/java/com/example/pomodoro2/features/tasks/domain/TaskInteractors.kt
+++ app/src/main/java/com/example/pomodoro2/features/tasks/domain/TaskInteractors.kt
@@ -4,7 +4,15 @@ import com.example.pomodoro2.interactors.*
 import com.example.pomodoro2.platform.interactor.Interactors
 
 /**
- * Definition of UseCase Set for each feature
+ * Definition of UseCase Set for Task
+ *
+ * Use Case of Task
+ * 1. Adding a subTask to a currently selected task.
+ * 2. Removing a subTask from a currently selected task.
+ * 3. Getting all activities for currently selected task.
+ * 4. Complete the selected subTask
+ * 5. Change the title of selected subTask
+ *
  */
 data class TaskInteractors (
     val createNewTaskUseCase: CreateNewTaskUseCase,
