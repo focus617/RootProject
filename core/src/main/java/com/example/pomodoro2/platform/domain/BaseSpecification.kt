bae3d18c2b62864cc59a797ad3a8fac3d9bf1777
Xu Zhiyong <13980668829@163.com>
2020-09-03 01:07:17 +0800

    Refact BaseSpecification and CompositeSpecification following design pattern.

--- core/src/main/java/com/example/pomodoro2/platform/domain/BaseSpecification.kt
+++ core/src/main/java/com/example/pomodoro2/platform/domain/BaseSpecification.kt
@@ -1,20 +1,36 @@
 package com.example.pomodoro2.platform.domain
 
+import com.example.pomodoro2.domain.model.Task
+
+/**
+ * @description 为组合对象声明接口，抽象所有类成员共有的默认行为
+ */
 abstract class BaseSpecification<T> {
     open fun isSatisfiedBy(t: T): Boolean = true
 }
 
 /**
+ * Example:
+ * @description 叶子节点对象，叶子节点没有子节点
+ *
+ * class ChildTaskSpec(private val parent: Task):BaseSpecification<Task>() {
+ *     override fun isSatisfiedBy(t: Task) = (t.getParent() == this.parent)
+ * }
+ */
+
+/**
  * Composite pattern for BaseSpecification
+ * @description 有枝节点对象，用来存储子部件
  *
  * @usage:
- *  specs = CompositeSpecification()
+ *  specs = CompositeSpecification<Task>()
  *  specs.add(ConcreteSpecification())
  *  specs.isSatisfiedBy(t)
+ *
  */
 class CompositeSpecification<T>: BaseSpecification<T>() {
 
-    private val specs = ArrayList<BaseSpecification<T>>()
+    private val specs = arrayListOf<BaseSpecification<T>>()
 
     fun getSpecs() = specs.toList()
 
