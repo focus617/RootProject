08d33f055d26385c36748b4c48867d62dd7aac5e
Xu Zhiyong <13980668829@163.com>
2020-09-04 21:20:50 +0800

    Add demo for Visitor Pattern.

---
+++ core/src/main/java/com/example/pomodoro2/platform/designpattern/visitor.kt
@@ -0,0 +1,120 @@
+package com.example.pomodoro2.platform.designpattern
+
+import com.example.pomodoro2.platform.logging.WithLogging
+
+
+/**
+ * @description 抽象访问者
+ */
+abstract class Visitor {
+    companion object : WithLogging()
+    abstract fun visitorConcreteElementA(concreteElementA: ConcreteElementA)
+    abstract fun visitorConcreteElementB(concreteElementB: ConcreteElementB)
+}
+
+
+/**
+ * @description 具体访问者A
+ */
+class ConcreteVisitorA : Visitor() {
+    override fun visitorConcreteElementA(concreteElementA: ConcreteElementA) {
+        LOG.info("${concreteElementA::class.java.simpleName}被${this::class.java.simpleName}访问")
+    }
+
+    override fun visitorConcreteElementB(concreteElementB: ConcreteElementB) {
+        LOG.info("${concreteElementB::class.java.simpleName}被${this::class.java.simpleName}访问")
+    }
+}
+
+/**
+ * @description 具体访问者B
+ */
+class ConcreteVisitorB : Visitor() {
+    override fun visitorConcreteElementA(concreteElementA: ConcreteElementA) {
+        LOG.info("${concreteElementA::class.java.simpleName}被${this::class.java.simpleName}访问")
+    }
+
+    override fun visitorConcreteElementB(concreteElementB: ConcreteElementB) {
+        LOG.info("${concreteElementB::class.java.simpleName}被${this::class.java.simpleName}访问")
+    }
+}
+
+
+/**
+ * @description 抽象元素
+ */
+abstract class Element {
+    //接受访问
+    abstract fun accept(visitor: Visitor)
+}
+
+
+/**
+ * @description 具体元素A
+ */
+class ConcreteElementA:Element() {
+    override fun accept(visitor: Visitor) {
+        visitor.visitorConcreteElementA(this)
+    }
+    /**
+     * 具体的其他方法
+     */
+    fun operationA(){
+
+    }
+}
+
+
+/**
+ * @description 具体元素B
+ */
+class ConcreteElementB : Element() {
+    override fun accept(visitor: Visitor) {
+        visitor.visitorConcreteElementB(this)
+    }
+
+    /**
+     * 具体的其他方法
+     */
+    fun operationB(){
+
+    }
+}
+
+
+/**
+ * @description 对象结构
+ */
+class ObjectStructure {
+    private val elements = arrayListOf<Element>()
+    fun attach(element: Element) {
+        elements.add(element)
+    }
+
+    fun detach(element: Element) {
+        elements.remove(element)
+    }
+
+    fun accept(visitor: Visitor) {
+        elements.forEach { element ->
+            element.accept(visitor)
+        }
+    }
+}
+
+    // 测试类
+class ClientVisitor {
+    companion object : WithLogging() {
+        @JvmStatic
+        fun main(vararg args: String) {
+            val objectStructure = ObjectStructure()
+            objectStructure.attach(ConcreteElementA())
+            objectStructure.attach(ConcreteElementB())
+            //成功时的反应
+            objectStructure.accept(ConcreteVisitorA())
+            //失败时的反应
+            objectStructure.accept(ConcreteVisitorB())
+
+        }
+    }
+}
