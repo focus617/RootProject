b91464a08c3847a019f62e8b22ab0a11e93d889b
Xu Zhiyong <13980668829@163.com>
2022-03-28 17:50:32 +0800

    test SurfaceView by drawing something.

--- app/src/main/java/com/focus617/tankwar/ui/game/GameFragment.kt
+++ app/src/main/java/com/focus617/tankwar/ui/game/GameFragment.kt
@@ -1,17 +1,17 @@
 package com.focus617.tankwar.ui.game
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.SurfaceView
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.lifecycle.ViewModelProvider
 import com.focus617.platform.uicontroller.BaseFragment
 import com.focus617.platform.view_util.setupSnackbar
 import com.focus617.tankwar.R
 import com.focus617.tankwar.databinding.FragmentGameBinding
 
-class GameFragment : BaseFragment() {
+class GameFragment : BaseFragment(), SurfaceHolder.Callback {
 
     private var _binding: FragmentGameBinding? = null
 
@@ -32,8 +32,7 @@ class GameFragment : BaseFragment() {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        surfaceView = binding.surfaceView
-
+        setupSurfaceView()
         welcome()
 
         return root
@@ -55,4 +54,45 @@ class GameFragment : BaseFragment() {
     private fun welcome() {
         viewModel.showSnackbarMessage(R.string.WelcomeMessage)
     }
+
+    private fun setupSurfaceView() {
+        surfaceView = binding.surfaceView
+        surfaceView.holder.addCallback(this)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder){
+        draw()
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+    }
+
+    private fun draw() {
+        // 创建画布
+        val canvas: Canvas = surfaceView.holder.lockCanvas()
+        // 画笔
+        val paint = Paint()
+
+        canvas.run {
+            // 设置画布背景
+            drawColor(Color.BLACK)
+
+            paint.color = Color.BLUE          //设置画笔颜色
+            drawRect(0F, 0F, 100F, 100F, paint)
+
+            save()
+            paint.color = Color.RED           //设置画笔颜色
+            paint.style = Paint.Style.FILL    //设置填充样式
+            paint.strokeWidth = 5F            //设置画笔宽度
+            rotate(90F, (width / 2).toFloat(), (height / 2).toFloat())
+            drawLine(0F, (height / 2).toFloat(), width.toFloat(), height.toFloat(), paint)
+            restore()
+        }
+
+        surfaceView.holder.unlockCanvasAndPost(canvas)
+    }
+
 }
