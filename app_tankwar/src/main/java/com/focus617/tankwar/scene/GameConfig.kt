8b9dae379a2b04d4e8e4e94b51896f2222b9ddea
Xu Zhiyong <13980668829@163.com>
2022-04-01 20:50:15 +0800

    Move all GameConfig object into GameScene.kt

--- app_tankwar/src/main/java/com/focus617/tankwar/scene/GameConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.focus617.tankwar.scene
-
-object GameConfig {
-    var BLOCK_WIDTH: Int = 0        // 游戏方格的宽度
-    var BLOCK_NUM_W: Int = 0        // 游戏场地横向方格的个数
-    var BLOCK_NUM_H: Int = 0        // 游戏场地纵向方格的个数
-}
--- app_tankwar/src/main/java/com/focus617/tankwar/scene/GameScene.kt
+++ app_tankwar/src/main/java/com/focus617/tankwar/scene/GameScene.kt
@@ -15,6 +15,12 @@ import com.focus617.tankwar.scene.components.Explode
 import com.focus617.tankwar.scene.components.Tank
 import java.util.*
 
+object GameConfig {
+    var BLOCK_WIDTH: Int = 0        // 游戏方格的宽度
+    var BLOCK_NUM_W: Int = 0        // 游戏场地横向方格的个数
+    var BLOCK_NUM_H: Int = 0        // 游戏场地纵向方格的个数
+}
+
 class GameScene(val context: Context) : IfScene, IfDraw {
 
     // 被绘制的对象集合
