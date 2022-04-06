c2ba46f27ee4a926a73b867049d6ac6932a7f83b
Xu Zhiyong <13980668829@163.com>
2022-04-06 14:02:02 +0800

    improve commandUnDo pattern in myLib: add redo logic.

--- myLib/src/main/java/com/focus617/mylib/designpattern/commandUnDo.kt
+++ myLib/src/main/java/com/focus617/mylib/designpattern/commandUnDo.kt
@@ -10,8 +10,8 @@ import com.focus617.mylib.logging.WithLogging
 /**
  * @description CommandUndo类用来将一个命令执行封装为一个对象，并声明执行do、undo操作的接口
  */
-abstract class CommandUnDo {
-    open fun doit(event: Event) {
+abstract class CommandUnDo(val event: Event) {
+    open fun execute() {
         CommandChain.add(this)
     }
 
@@ -19,28 +19,35 @@ abstract class CommandUnDo {
 }
 
 /**
- * @description 另一种责任链实现方式：具有统一的处理者，
- * 将请求依次传递给持有的处理者
- * 处理者负责如果可处理该请求，就处理之，否则返回不能处理的通知；
- * 再由本类将该请求转发给它的后继者。
+ * @description 命令链，负责保存
  */
 object CommandChain : ILoggable {
     private val LOG = logger()
+
     private val commands = ArrayList<CommandUnDo>()
+    private var cursor: Int = 0
 
     fun add(cmd: CommandUnDo) {
         commands.add(cmd)
+        cursor++
     }
 
     fun undo() {
-        if (commands.size == 0) {
+        if (cursor == 0) {
             LOG.info("${this::class.java.simpleName}: nothing to undo\n")
             return
         }
 
-        val cmd = commands[commands.size - 1]
-        cmd.undo()
-        commands.remove(cmd)
+        commands[cursor - 1].undo()
+        cursor--
+    }
+
+    fun redo() {
+        if(cursor == commands.size){
+            LOG.info("${this::class.java.simpleName}: nothing to redo\n")
+            return
+        }
+        commands[cursor++].execute()
     }
 }
 
@@ -49,13 +56,13 @@ object Content{
     var msg: String = "This is testing content."
 }
 
-class AppendCmd() : CommandUnDo() {
+class AppendCmd(event: Event) : CommandUnDo(event) {
     companion object : WithLogging()
 
     val stringToAppend = "Append_Content"
 
-    override fun doit(event: Event) {
-        super.doit(event)
+    override fun execute() {
+        super.execute()
         LOG.info("${this::class.java.simpleName}.doit is called")
         Content.msg += stringToAppend
     }
@@ -67,14 +74,14 @@ class AppendCmd() : CommandUnDo() {
 
 }
 
-class DeleteCmd() : CommandUnDo() {
+class DeleteCmd(event: Event) : CommandUnDo(event) {
     companion object : WithLogging()
 
     // Command负责记录和保存自己操作的变化量
     var stringToDelete = ""
 
-    override fun doit(event: Event) {
-        super.doit(event)
+    override fun execute() {
+        super.execute()
         LOG.info("${this::class.java.simpleName}.doit is called")
         stringToDelete = Content.msg
         Content.msg = ""
@@ -105,12 +112,12 @@ class ClientCommandUndoTesting {
             val invoker = InvokerUndo()
             val event = AppStartEvent(invoker)
 
-            LOG.info(Content.msg + '\n')
+            LOG.info("Origin:" + Content.msg + '\n')
             //进行操作
-            AppendCmd().doit(event)
+            AppendCmd(event).execute()
             LOG.info(Content.msg + '\n')
 
-            DeleteCmd().doit(event)
+            DeleteCmd(event).execute()
             LOG.info(Content.msg + '\n')
 
             // 测试undo
@@ -119,6 +126,14 @@ class ClientCommandUndoTesting {
             CommandChain.undo()
             LOG.info(Content.msg + '\n')
             CommandChain.undo()
+            LOG.info(Content.msg + '\n')
+
+            // 测试redo
+            CommandChain.redo()
+            LOG.info(Content.msg + '\n')
+            CommandChain.redo()
+            LOG.info(Content.msg + '\n')
+            CommandChain.redo()
             LOG.info(Content.msg + '\n')
         }
     }
