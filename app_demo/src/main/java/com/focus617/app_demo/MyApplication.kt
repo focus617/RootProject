64027517d19b20fa6752724acd86899adc4a5f85
Xu Zhiyong <13980668829@163.com>
2022-04-30 21:40:14 +0800

    rollback this abnormal change.

--- app_demo/src/main/java/com/focus617/app_demo/MyApplication.kt
+++ app_demo/src/main/java/com/focus617/app_demo/MyApplication.kt
@@ -23,7 +23,7 @@ class MyApplication : BaseApplication() {
 
         // 初始化引擎
         // is3D是选择2D，或3D的总开关
-        gameEngine = Sandbox(is3D = false)
+        gameEngine = Sandbox(is3D = true)
     }
 
     override fun onTerminate() {
