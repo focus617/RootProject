86ba422b589547227a082101ad287ff6654cef15
Xu Zhiyong <13980668829@163.com>
2022-05-02 23:02:06 +0800

    Create XGLTextureCubeMap class.

--- app_demo/src/main/java/com/focus617/app_demo/engine/d3/XGLRenderer3D.kt
+++ app_demo/src/main/java/com/focus617/app_demo/engine/d3/XGLRenderer3D.kt
@@ -5,23 +5,20 @@ import android.opengl.GLSurfaceView
 import com.focus617.app_demo.engine.XGLContext
 import com.focus617.app_demo.renderer.XGLShader
 import com.focus617.app_demo.renderer.XGLShaderBuilder
-import com.focus617.app_demo.renderer.XGLTextureBuilder
+import com.focus617.app_demo.renderer.XGLTextureCubeMap
 import com.focus617.app_demo.renderer.XGLVertexArray
 import com.focus617.core.engine.baseDataType.Color
 import com.focus617.core.engine.math.Vector2
 import com.focus617.core.engine.math.Vector3
-import com.focus617.core.engine.math.Vector4
 import com.focus617.core.engine.objects.DrawableObject
 import com.focus617.core.engine.objects.IfDrawable
 import com.focus617.core.engine.renderer.RenderCommand
-import com.focus617.core.engine.renderer.Texture2D
 import com.focus617.core.engine.renderer.VertexArray
 import com.focus617.core.engine.renderer.XRenderer
 import com.focus617.core.engine.scene.Camera
 import com.focus617.core.engine.scene.PerspectiveCamera
 import com.focus617.core.engine.scene.PerspectiveCameraController
 import com.focus617.core.engine.scene.Scene
-import com.focus617.platform.helper.TextureHelper
 import java.io.Closeable
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -48,23 +45,12 @@ class XGLRenderer3D(
     }
 
     private fun initTextureForScene() {
-        val texture = XGLTextureBuilder.createTexture(
-            context,
-            objectTextureName
-        )!!
-        scene.register(objectTextureName, texture)
-
-        val textureLogo = XGLTextureBuilder.createTexture(
-            context,
-            logoTextureName
-        )!!
-        scene.register(logoTextureName, textureLogo)
-
-        TextureHelper.skyboxTexture = TextureHelper.loadCubeMap(
+        val texture = XGLTextureCubeMap(
             context,
             SHADER_PATH,
-            arrayOf("left.png", "right.png", "bottom.png","top.png","front.png","back.png")
+            arrayOf("left.png", "right.png", "bottom.png", "top.png", "front.png", "back.png")
         )
+        scene.register(objectTextureName, texture)
     }
 
     private fun initShaderForScene(context: Context) {
@@ -73,8 +59,8 @@ class XGLRenderer3D(
             context,
             "$SHADER_PATH/$SHADER_FILE"
         ) as XGLShader
-        //shader.bind()
-        //shader.setInt("u_Texture", 0)
+        shader.bind()
+        shader.setInt("u_TextureUnit", 0)
         scene.mShaderLibrary.add(shader)
     }
 
@@ -142,7 +128,7 @@ class XGLRenderer3D(
         )
         //LOG.info(XMatrix.toString(transform, matrixName = "ModelMatrix"))
 
-        (scene.texture(objectTextureName)!! as Texture2D).bind()
+        scene.texture(objectTextureName)!!.bind()
         submit(sVertexArray, transform)
 
         // This texture has transparent alpha for part of image
@@ -170,7 +156,7 @@ class XGLRenderer3D(
         val shader = scene.mShaderLibrary.get(SHADER_FILE)
         (shader as XGLShader).bind()
         shader.setMat4("u_ModelMatrix", transform)
-        shader.setFloat4("u_Color", WHITE)
+        //shader.setFloat4("u_Color", WHITE)
 
         vertexArray.bind()
         RenderCommand.drawIndexed(vertexArray)
@@ -186,16 +172,8 @@ class XGLRenderer3D(
         private val SHADER_PATH = "Cube"
         private val SHADER_FILE = "SkyBox.glsl"
 
-        private val TEXTURE_PATH = "SquareWithTexture"
-        private val TEXTURE_FILE = "Checkerboard.png"
-        private val TEXTURE_LOGO_FILE = "Logo.png"
+        private val objectTextureName = "SkyCubeMapTexture"
 
-        val logoTextureName = "${TEXTURE_PATH}/${TEXTURE_LOGO_FILE}"
-        val objectTextureName = "${TEXTURE_PATH}/${TEXTURE_FILE}"
-
-        val WHITE = Vector4(1.0f, 1.0f, 1.0f, 1.0f)
-        val RED = Vector4(0.8f, 0.3f, 0.2f, 1.0f)
-        val BLUE = Vector4(0.2f, 0.3f, 0.8f, 1.0f)
     }
 
 }
