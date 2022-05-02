902b39bb0c2ef7e2ff22f8ed21e9fa57710f6c82
Xu Zhiyong <13980668829@163.com>
2022-05-02 21:45:09 +0800

    Move TextureHelper out of XGLTexture2D, as well as refactor the loadCubeMap to load BMP from assets file.

--- app_demo/src/main/java/com/focus617/app_demo/renderer/XGLTexture2D.kt
+++ app_demo/src/main/java/com/focus617/app_demo/renderer/XGLTexture2D.kt
@@ -2,14 +2,12 @@ package com.focus617.app_demo.renderer
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.opengl.GLES20.*
 import android.opengl.GLES30
 import android.opengl.GLES30.GL_RGBA8
 import android.opengl.GLES31
-import android.opengl.GLUtils
 import com.focus617.core.engine.renderer.Texture2D
-import java.io.IOException
+import com.focus617.platform.helper.TextureHelper
 import java.nio.Buffer
 import java.nio.ByteBuffer
 
@@ -34,13 +32,13 @@ class XGLTexture2D private constructor() : Texture2D() {
 
     /** 基于Assets中的文件构造 */
     constructor(context: Context, filePath: String) : this() {
-        val bitmap = loadTextureFromFile(context, filePath)
+        val bitmap = TextureHelper.loadTextureFromFile(context, filePath)
         bitmap?.apply { initTexture(bitmap) }
     }
 
     /** 基于Resource/raw中的文件构造 */
     constructor(context: Context, resourceId: Int) : this() {
-        val bitmap = loadTextureFromResource(context, resourceId)
+        val bitmap = TextureHelper.loadTextureFromResource(context, resourceId)
         bitmap?.apply { initTexture(bitmap) }
     }
 
@@ -150,7 +148,7 @@ class XGLTexture2D private constructor() : Texture2D() {
         GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, textureObjectId)
 
         //绑定纹理单元与sampler
-        GLES31.glBindSampler(0, samplers[0])
+        GLES31.glBindSampler(0, TextureHelper.samplers[0])
 
         // Set
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
@@ -193,254 +191,4 @@ class XGLTexture2D private constructor() : Texture2D() {
         return textureObjectId
     }
 
-
-    companion object TextureHelper {
-        val TAG = "XGLShader"
-        private var samplers = IntArray(1) //存放Samplers id的成员变量数组
-
-        init {
-            initSampler()
-        }
-
-        private fun initSampler() {        //初始化Sampler对象的方法
-            GLES31.glGenSamplers(1, samplers, 0) //生成Samplers id
-            GLES31.glSamplerParameterf(
-                samplers[0],
-                GLES31.GL_TEXTURE_MIN_FILTER,
-                GLES31.GL_LINEAR_MIPMAP_LINEAR.toFloat()
-            ) //设置MIN采样方式
-            GLES31.glSamplerParameterf(
-                samplers[0],
-                GLES31.GL_TEXTURE_MAG_FILTER,
-                GLES31.GL_LINEAR.toFloat()
-            ) //设置MAG采样方式
-            GLES31.glSamplerParameterf(
-                samplers[0],
-                GLES31.GL_TEXTURE_WRAP_S,
-                GLES31.GL_REPEAT.toFloat()
-            ) //设置S轴拉伸方式
-            GLES31.glSamplerParameterf(
-                samplers[0],
-                GLES31.GL_TEXTURE_WRAP_T,
-                GLES31.GL_REPEAT.toFloat()
-            ) //设置T轴拉伸方式
-        }
-
-        /**
-         * Loads a texture from a resource ID, returning the OpenGL ID for that
-         * texture. Returns 0 if the load failed.
-         *
-         * @param context
-         * @param resourceId
-         * @return
-         */
-        private fun loadTextureFromResource(context: Context, resourceId: Int): Bitmap? {
-            LOG.info("${TAG}: load texture from file: $resourceId")
-
-            var bitmap: Bitmap? = null
-            val options = BitmapFactory.Options()
-            options.inScaled = false
-
-            // Read in the resource
-            bitmap = BitmapFactory.decodeResource(
-                context.resources, resourceId, options
-            )
-            if (bitmap == null) {
-                LOG.error("Resource ID $resourceId could not be decoded.")
-            }
-            return bitmap
-        }
-
-        /**
-         * Loads a texture from a file, returning the OpenGL ID for that
-         * texture. Returns 0 if the load failed.
-         *
-         * @param context
-         * @param filePath
-         * @return
-         */
-        private fun loadTextureFromFile(context: Context, filePath: String): Bitmap? {
-            LOG.info("${TAG}: load texture from file: $filePath")
-
-            var bitmap: Bitmap? = null
-
-            val options = BitmapFactory.Options()
-            options.inScaled = false
-
-            try {
-                val inputStream = context.resources.assets.open(filePath)
-                // Read in the resource
-                bitmap = BitmapFactory.decodeStream(inputStream)
-                if (bitmap == null) {
-                    LOG.error("$filePath could not be decoded.")
-                }
-            } catch (e: IOException) {
-                throw RuntimeException("Could not open shader file: $filePath $ e")
-            }
-            return bitmap
-        }
-
-        /**
-         * Loads a cubemap texture from the provided resources and returns the
-         * texture ID. Returns 0 if the load failed.
-         *
-         * @param context
-         * @param cubeResources
-         * An array of resources corresponding to the cube map. Should be
-         * provided in this order: left, right, bottom, top, front, back.
-         * @return
-         */
-        fun loadCubeMap(context: Context, cubeResources: IntArray): Int {
-            val textureObjectIds = IntArray(1)
-            GLES31.glGenTextures(1, textureObjectIds, 0)
-            if (textureObjectIds[0] == 0) {
-                LOG.warn("Could not generate a new OpenGL texture object.")
-                return 0
-            }
-            val options = BitmapFactory.Options()
-            options.inScaled = false
-
-            val cubeBitmaps = arrayOfNulls<Bitmap>(6)
-
-            for (i in 0..5) {
-                cubeBitmaps[i] = BitmapFactory.decodeResource(
-                    context.resources,
-                    cubeResources[i], options
-                )
-                if (cubeBitmaps[i] == null) {
-                    LOG.warn("Resource ID ${cubeResources[i]} could not be decoded.")
-                    GLES31.glDeleteTextures(1, textureObjectIds, 0)
-                    return 0
-                }
-            }
-
-            // Linear filtering for minification and magnification
-            GLES31.glBindTexture(GLES31.GL_TEXTURE_CUBE_MAP, textureObjectIds[0])
-            // 因为每个立方体贴图的纹理都总是从同一个视点被观察，不必使用MIP
-            // 所以我们只使用常规的双线性过滤，也能节省纹理内存。
-            GLES31.glTexParameteri(
-                GLES31.GL_TEXTURE_CUBE_MAP,
-                GLES31.GL_TEXTURE_MIN_FILTER,
-                GLES31.GL_LINEAR
-            )
-            GLES31.glTexParameteri(
-                GLES31.GL_TEXTURE_CUBE_MAP,
-                GLES31.GL_TEXTURE_MAG_FILTER,
-                GLES31.GL_LINEAR
-            )
-            // 立方体贴图的惯例是： 在立方体内部使用左手坐标系统，而在立方体外部使用右手坐标系统
-            // 要以左、右、下、上、前和后的顺序把每张图像与其对应的立方体贴图的面关联起来
-            GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, cubeBitmaps[0], 0)
-            GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, cubeBitmaps[1], 0)
-            GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, cubeBitmaps[2], 0)
-            GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, cubeBitmaps[3], 0)
-            GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, cubeBitmaps[4], 0)
-            GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, cubeBitmaps[5], 0)
-            GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, 0)
-
-            for (bitmap in cubeBitmaps) {
-                bitmap!!.recycle()
-            }
-
-            return textureObjectIds[0]
-        }
-
-        // 多重纹理(multitexturing)
-        enum class FilterMode {
-            NEAREST_NEIGHBOUR,
-            BILINEAR,
-            BILINEAR_WITH_MIPMAPS,
-            TRILINEAR,
-            ANISOTROPIC
-        }
-
-        private const val TEXTURE_MAX_ANISOTROPY_EXT = 0x84FE
-
-        fun adjustTextureFilters(
-            textureId: Int,
-            filterMode: FilterMode,
-            supportsAnisotropicFiltering: Boolean,
-            maxAnisotropy: Float
-        ) {
-            GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, textureId)
-
-            if (supportsAnisotropicFiltering) {
-                if (filterMode == FilterMode.ANISOTROPIC) {
-                    GLES31.glTexParameterf(
-                        GLES31.GL_TEXTURE_2D,
-                        TEXTURE_MAX_ANISOTROPY_EXT,
-                        maxAnisotropy
-                    )
-                } else {
-                    GLES31.glTexParameterf(GLES31.GL_TEXTURE_2D, TEXTURE_MAX_ANISOTROPY_EXT, 1.0f)
-                }
-            }
-
-            when (filterMode) {
-                FilterMode.NEAREST_NEIGHBOUR -> {
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MIN_FILTER,
-                        GLES31.GL_NEAREST
-                    )
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MAG_FILTER,
-                        GLES31.GL_NEAREST
-                    )
-                }
-                FilterMode.BILINEAR -> {
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MIN_FILTER,
-                        GLES31.GL_LINEAR
-                    )
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MAG_FILTER,
-                        GLES31.GL_LINEAR
-                    )
-                }
-                FilterMode.BILINEAR_WITH_MIPMAPS -> {
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MIN_FILTER,
-                        GLES31.GL_LINEAR_MIPMAP_NEAREST
-                    )
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MAG_FILTER,
-                        GLES31.GL_LINEAR
-                    )
-                }
-                FilterMode.TRILINEAR -> {
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MIN_FILTER,
-                        GLES31.GL_LINEAR_MIPMAP_LINEAR
-                    )
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MAG_FILTER,
-                        GLES31.GL_LINEAR
-                    )
-                }
-                FilterMode.ANISOTROPIC -> {
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MIN_FILTER,
-                        GLES31.GL_LINEAR_MIPMAP_LINEAR
-                    )
-                    GLES31.glTexParameteri(
-                        GLES31.GL_TEXTURE_2D,
-                        GLES31.GL_TEXTURE_MAG_FILTER,
-                        GLES31.GL_LINEAR
-                    )
-                }
-            }
-
-            GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, 0)
-        }
-
-    }
 }
--- myAndroidLibrary/src/main/java/com/focus617/platform/helper/TextureHelper.kt
+++ myAndroidLibrary/src/main/java/com/focus617/platform/helper/TextureHelper.kt
@@ -3,30 +3,42 @@ package com.focus617.platform.helper
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.opengl.GLES31.*
+import android.opengl.GLES31
 import android.opengl.GLUtils
 import timber.log.Timber
 import java.io.IOException
 
-
 object TextureHelper {
+    val TAG = "TextureHelper"
 
-    private var samplers = IntArray(1) //存放Samplers id的成员变量数组
+    var samplers = IntArray(1) //存放Samplers id的成员变量数组
 
     init {
         initSampler()
     }
 
     private fun initSampler() {        //初始化Sampler对象的方法
-        glGenSamplers(1, samplers, 0) //生成Samplers id
-        glSamplerParameterf(
+        GLES31.glGenSamplers(1, samplers, 0) //生成Samplers id
+        GLES31.glSamplerParameterf(
             samplers[0],
-            GL_TEXTURE_MIN_FILTER,
-            GL_LINEAR_MIPMAP_LINEAR.toFloat()
+            GLES31.GL_TEXTURE_MIN_FILTER,
+            GLES31.GL_LINEAR_MIPMAP_LINEAR.toFloat()
         ) //设置MIN采样方式
-        glSamplerParameterf(samplers[0], GL_TEXTURE_MAG_FILTER, GL_LINEAR.toFloat()) //设置MAG采样方式
-        glSamplerParameterf(samplers[0], GL_TEXTURE_WRAP_S, GL_REPEAT.toFloat()) //设置S轴拉伸方式
-        glSamplerParameterf(samplers[0], GL_TEXTURE_WRAP_T, GL_REPEAT.toFloat()) //设置T轴拉伸方式
+        GLES31.glSamplerParameterf(
+            samplers[0],
+            GLES31.GL_TEXTURE_MAG_FILTER,
+            GLES31.GL_LINEAR.toFloat()
+        ) //设置MAG采样方式
+        GLES31.glSamplerParameterf(
+            samplers[0],
+            GLES31.GL_TEXTURE_WRAP_S,
+            GLES31.GL_REPEAT.toFloat()
+        ) //设置S轴拉伸方式
+        GLES31.glSamplerParameterf(
+            samplers[0],
+            GLES31.GL_TEXTURE_WRAP_T,
+            GLES31.GL_REPEAT.toFloat()
+        ) //设置T轴拉伸方式
     }
 
     /**
@@ -37,57 +49,21 @@ object TextureHelper {
      * @param resourceId
      * @return
      */
-    fun loadTexture(context: Context, resourceId: Int): Int {
+    fun loadTextureFromResource(context: Context, resourceId: Int): Bitmap? {
+        Timber.i("${TAG}: load texture from resource: $resourceId")
 
-        Timber.d("loadTexture($resourceId)")
-
-        val textureObjectIds = IntArray(1)
-        glGenTextures(1, textureObjectIds, 0)
-        if (textureObjectIds[0] == 0) {
-            Timber.w("Could not generate a new OpenGL texture object.")
-            return 0
-        }
+        var bitmap: Bitmap? = null
         val options = BitmapFactory.Options()
         options.inScaled = false
 
         // Read in the resource
-        val bitmap = BitmapFactory.decodeResource(
+        bitmap = BitmapFactory.decodeResource(
             context.resources, resourceId, options
         )
         if (bitmap == null) {
-            Timber.w("Resource ID $resourceId could not be decoded.")
-            glDeleteTextures(1, textureObjectIds, 0)
-            return 0
+            Timber.e("$TAG: Resource ID $resourceId could not be decoded.")
         }
-        // Bind to the texture in OpenGL
-        glBindTexture(GL_TEXTURE_2D, textureObjectIds[0])
-
-        //绑定纹理单元与sampler
-        glBindSampler(0, samplers[0])
-        // Set filtering: a default must be set, or the texture will be black.
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-//        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-
-        // Load the bitmap into the bound texture.
-        GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0)
-
-        // Note: Following code may cause an error to be reported in the
-        // ADB log as follows: E/IMGSRV(20095): :0: HardwareMipGen:
-        // Failed to generate texture mipmap levels (error=3)
-        // No OpenGL error will be encountered (glGetError() will return
-        // 0). If this happens, just squash the source image to be
-        // square. It will look the same because of texture coordinates,
-        // and mipmap generation will work.
-        glGenerateMipmap(GL_TEXTURE_2D)
-
-        // Recycle the bitmap, since its data has been loaded into
-        // OpenGL.
-        bitmap.recycle()
-
-        // Unbind from the texture.
-        glBindTexture(GL_TEXTURE_2D, 0)
-
-        return textureObjectIds[0]
+        return bitmap
     }
 
     /**
@@ -98,105 +74,86 @@ object TextureHelper {
      * @param filePath
      * @return
      */
-    fun loadTextureFromFile(context: Context, filePath: String): Int {
+    fun loadTextureFromFile(context: Context, filePath: String): Bitmap? {
+        Timber.i("${TAG}: load texture from file: $filePath")
 
-        Timber.d("loadTextureFromFile(): $filePath")
+        var bitmap: Bitmap? = null
 
-        val textureObjectIds = IntArray(1)
-        glGenTextures(1, textureObjectIds, 0)
-        if (textureObjectIds[0] == 0) {
-            Timber.w("Could not generate a new OpenGL texture object.")
-            return 0
-        }
         val options = BitmapFactory.Options()
         options.inScaled = false
 
         try {
             val inputStream = context.resources.assets.open(filePath)
             // Read in the resource
-            val bitmap = BitmapFactory.decodeStream(inputStream)
+            bitmap = BitmapFactory.decodeStream(inputStream)
             if (bitmap == null) {
-                Timber.w("$filePath could not be decoded.")
-                glDeleteTextures(1, textureObjectIds, 0)
-                return 0
+                Timber.e("$TAG: $filePath could not be decoded.")
             }
-            // Bind to the texture in OpenGL
-            glBindTexture(GL_TEXTURE_2D, textureObjectIds[0])
-
-            //绑定纹理单元与sampler
-            glBindSampler(0, samplers[0])
-
-            // Load the bitmap into the bound texture.
-            GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0)
-
-            // Note: Following code may cause an error to be reported in the
-            // ADB log as follows: E/IMGSRV(20095): :0: HardwareMipGen:
-            // Failed to generate texture mipmap levels (error=3)
-            // No OpenGL error will be encountered (glGetError() will return
-            // 0). If this happens, just squash the source image to be
-            // square. It will look the same because of texture coordinates,
-            // and mipmap generation will work.
-            glGenerateMipmap(GL_TEXTURE_2D)
-
-            // Recycle the bitmap, since its data has been loaded into
-            // OpenGL.
-            bitmap.recycle()
-
         } catch (e: IOException) {
             throw RuntimeException("Could not open shader file: $filePath $ e")
         }
-
-        // Unbind from the texture.
-        glBindTexture(GL_TEXTURE_2D, 0)
-
-        return textureObjectIds[0]
+        return bitmap
     }
+
+    //TODO: define XGLTexture3D class
+    var skyboxTexture: Int = 0
 
     /**
      * Loads a cubemap texture from the provided resources and returns the
      * texture ID. Returns 0 if the load failed.
      *
      * @param context
-     * @param cubeResources
-     * An array of resources corresponding to the cube map. Should be
+     * @param path
+     * An array of texture file name corresponding to the cube map. Should be
      * provided in this order: left, right, bottom, top, front, back.
      * @return
      */
-    fun loadCubeMap(context: Context, cubeResources: IntArray): Int {
+    fun loadCubeMap(
+        context: Context,
+        path: String,
+        files: Array<String>
+    ): Int {
         val textureObjectIds = IntArray(1)
-        glGenTextures(1, textureObjectIds, 0)
+        GLES31.glGenTextures(1, textureObjectIds, 0)
         if (textureObjectIds[0] == 0) {
-            Timber.w("Could not generate a new OpenGL texture object.")
+            Timber.w("$TAG: Could not generate a new OpenGL texture object.")
             return 0
         }
-        val options = BitmapFactory.Options()
-        options.inScaled = false
 
         val cubeBitmaps = arrayOfNulls<Bitmap>(6)
 
         for (i in 0..5) {
-            cubeBitmaps[i] = BitmapFactory.decodeResource(
-                context.resources,
-                cubeResources[i], options
-            )
+            cubeBitmaps[i] = loadTextureFromFile(context, "$path/${files[i]}")
             if (cubeBitmaps[i] == null) {
-                Timber.w("Resource ID ${cubeResources[i]} could not be decoded.")
-                glDeleteTextures(1, textureObjectIds, 0)
+                Timber.w("$TAG: Resource ID ${files[i]} could not be decoded.")
+                GLES31.glDeleteTextures(1, textureObjectIds, 0)
                 return 0
             }
         }
 
         // Linear filtering for minification and magnification
-        glBindTexture(GL_TEXTURE_CUBE_MAP, textureObjectIds[0])
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        GLUtils.texImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, cubeBitmaps[0], 0)
-        GLUtils.texImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, cubeBitmaps[1], 0)
-        GLUtils.texImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, cubeBitmaps[2], 0)
-        GLUtils.texImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, cubeBitmaps[3], 0)
-        GLUtils.texImage2D(GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, cubeBitmaps[4], 0)
-        GLUtils.texImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, cubeBitmaps[5], 0)
-        glBindTexture(GL_TEXTURE_2D, 0)
+        GLES31.glBindTexture(GLES31.GL_TEXTURE_CUBE_MAP, textureObjectIds[0])
+        // 因为每个立方体贴图的纹理都总是从同一个视点被观察，不必使用MIP
+        // 所以我们只使用常规的双线性过滤，也能节省纹理内存。
+        GLES31.glTexParameteri(
+            GLES31.GL_TEXTURE_CUBE_MAP,
+            GLES31.GL_TEXTURE_MIN_FILTER,
+            GLES31.GL_LINEAR
+        )
+        GLES31.glTexParameteri(
+            GLES31.GL_TEXTURE_CUBE_MAP,
+            GLES31.GL_TEXTURE_MAG_FILTER,
+            GLES31.GL_LINEAR
+        )
+        // 立方体贴图的惯例是： 在立方体内部使用左手坐标系统，而在立方体外部使用右手坐标系统
+        // 要以左、右、下、上、前和后的顺序把每张图像与其对应的立方体贴图的面关联起来
+        GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_NEGATIVE_X, 0, cubeBitmaps[0], 0)
+        GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_POSITIVE_X, 0, cubeBitmaps[1], 0)
+        GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_NEGATIVE_Y, 0, cubeBitmaps[2], 0)
+        GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_POSITIVE_Y, 0, cubeBitmaps[3], 0)
+        GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_NEGATIVE_Z, 0, cubeBitmaps[4], 0)
+        GLUtils.texImage2D(GLES31.GL_TEXTURE_CUBE_MAP_POSITIVE_Z, 0, cubeBitmaps[5], 0)
+        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, 0)
 
         for (bitmap in cubeBitmaps) {
             bitmap!!.recycle()
@@ -222,40 +179,84 @@ object TextureHelper {
         supportsAnisotropicFiltering: Boolean,
         maxAnisotropy: Float
     ) {
-        glBindTexture(GL_TEXTURE_2D, textureId)
+        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, textureId)
 
         if (supportsAnisotropicFiltering) {
             if (filterMode == FilterMode.ANISOTROPIC) {
-                glTexParameterf(GL_TEXTURE_2D, TEXTURE_MAX_ANISOTROPY_EXT, maxAnisotropy)
+                GLES31.glTexParameterf(
+                    GLES31.GL_TEXTURE_2D,
+                    TEXTURE_MAX_ANISOTROPY_EXT,
+                    maxAnisotropy
+                )
             } else {
-                glTexParameterf(GL_TEXTURE_2D, TEXTURE_MAX_ANISOTROPY_EXT, 1.0f)
+                GLES31.glTexParameterf(GLES31.GL_TEXTURE_2D, TEXTURE_MAX_ANISOTROPY_EXT, 1.0f)
             }
         }
 
         when (filterMode) {
             FilterMode.NEAREST_NEIGHBOUR -> {
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MIN_FILTER,
+                    GLES31.GL_NEAREST
+                )
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MAG_FILTER,
+                    GLES31.GL_NEAREST
+                )
             }
             FilterMode.BILINEAR -> {
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MIN_FILTER,
+                    GLES31.GL_LINEAR
+                )
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MAG_FILTER,
+                    GLES31.GL_LINEAR
+                )
             }
             FilterMode.BILINEAR_WITH_MIPMAPS -> {
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST)
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MIN_FILTER,
+                    GLES31.GL_LINEAR_MIPMAP_NEAREST
+                )
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MAG_FILTER,
+                    GLES31.GL_LINEAR
+                )
             }
             FilterMode.TRILINEAR -> {
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MIN_FILTER,
+                    GLES31.GL_LINEAR_MIPMAP_LINEAR
+                )
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MAG_FILTER,
+                    GLES31.GL_LINEAR
+                )
             }
             FilterMode.ANISOTROPIC -> {
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR)
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MIN_FILTER,
+                    GLES31.GL_LINEAR_MIPMAP_LINEAR
+                )
+                GLES31.glTexParameteri(
+                    GLES31.GL_TEXTURE_2D,
+                    GLES31.GL_TEXTURE_MAG_FILTER,
+                    GLES31.GL_LINEAR
+                )
             }
         }
 
-        glBindTexture(GL_TEXTURE_2D, 0)
+        GLES31.glBindTexture(GLES31.GL_TEXTURE_2D, 0)
     }
 
 }
