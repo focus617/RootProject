bed3cc00eba5035e821d28113ebc65e3b12d51a4
Xu Zhiyong <13980668829@163.com>
2022-05-07 10:08:28 +0800

    move touch event handlers to Sandbox.

--- app_demo/src/main/java/com/focus617/app_demo/terrain/TerrainLayer.kt
+++ app_demo/src/main/java/com/focus617/app_demo/terrain/TerrainLayer.kt
@@ -6,8 +6,6 @@ import com.focus617.core.engine.core.TimeStep
 import com.focus617.core.engine.math.Vector3
 import com.focus617.core.platform.event.base.Event
 import com.focus617.core.platform.event.base.EventDispatcher
-import com.focus617.core.platform.event.base.EventType
-import com.focus617.core.platform.event.screenTouchEvents.TouchDragEvent
 
 class TerrainLayer(name: String, private val scene: XGLScene3D, val is3D: Boolean) : Layer(name) {
     private val eventDispatcher = EventDispatcher()
@@ -32,12 +30,10 @@ class TerrainLayer(name: String, private val scene: XGLScene3D, val is3D: Boolea
 
     override fun onAttach() {
         LOG.info("${this.mDebugName} onAttach()")
-        registerEventHandlers()
     }
 
     override fun onDetach() {
         LOG.info("${this.mDebugName} onDetach")
-        unRegisterEventHandlers()
     }
 
     override fun onUpdate(timeStep: TimeStep) {
@@ -52,47 +48,5 @@ class TerrainLayer(name: String, private val scene: XGLScene3D, val is3D: Boolea
         LOG.info("${this.mDebugName} closed")
         eventDispatcher.close()
     }
-
-    // 处理各种触屏事件，例如可能引起相机位置变化的事件
-    private fun registerEventHandlers() {
-        eventDispatcher.register(EventType.TouchDrag) { event ->
-            val e: TouchDragEvent = event as TouchDragEvent
-            LOG.info("${this.mDebugName}: ${e.name} from ${e.source} received")
-//                LOG.info("It's type is ${e.eventType}")
-//                LOG.info("It's was submit at ${DateHelper.timeStampAsStr(e.timestamp)}")
-//                LOG.info("Current position is (${e.x}, ${e.y})\n")
-            val hasConsumed = scene.mCameraController.onEvent(event)
-            hasConsumed
-        }
-
-        eventDispatcher.register(EventType.TouchPress) { event ->
-            val hasConsumed = scene.mCameraController.onEvent(event)
-            hasConsumed
-        }
-
-        eventDispatcher.register(EventType.PinchStart) { event ->
-            val hasConsumed = scene.mCameraController.onEvent(event)
-            hasConsumed
-        }
-
-        eventDispatcher.register(EventType.PinchEnd) { event ->
-            val hasConsumed = scene.mCameraController.onEvent(event)
-            hasConsumed
-        }
-
-        eventDispatcher.register(EventType.Pinch) { event ->
-            val hasConsumed = scene.mCameraController.onEvent(event)
-            hasConsumed
-        }
-    }
-
-    private fun unRegisterEventHandlers() {
-        eventDispatcher.unRegister(EventType.TouchDrag)
-        eventDispatcher.unRegister(EventType.TouchPress)
-        eventDispatcher.unRegister(EventType.PinchStart)
-        eventDispatcher.unRegister(EventType.PinchEnd)
-        eventDispatcher.unRegister(EventType.Pinch)
-    }
-
 
 }
