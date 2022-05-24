9dcf6eea17bb1a0f9e3c06346dd04b03df642fc2
Xu Zhiyong <13980668829@163.com>
2022-05-24 14:25:02 +0800

    Define Relationship component.

--- core/src/main/java/com/focus617/core/ecs/mine/component/Relationship.kt
+++ core/src/main/java/com/focus617/core/ecs/mine/component/Relationship.kt
@@ -1,9 +1,15 @@
 package com.focus617.core.ecs.mine.component
 
+/**
+ * 表达层级关系
+ * 里面的数据值（Int类型） = Entity.id，值为-1表示无效值
+ */
 data class Relationship(
     var childrenNumber: Int = 0,
-    var first: Int = -1,    // Entity.id
-    var prev: Int = -1,     // Entity.id
-    var next: Int = -1,     // Entity.id
-    var parent: Int = -1,   // Entity.id
+    var first: Int = -1,   // children链表的表头
+    var last: Int = -1,    // children链表的表尾
+
+    var parent: Int = -1,
+    var prev: Int = -1,    // 链表的前一个
+    var next: Int = -1     // 链表的后一个
 )
