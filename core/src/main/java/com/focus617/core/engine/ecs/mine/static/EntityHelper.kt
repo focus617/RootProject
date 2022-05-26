2142454f589535092cf18d2fd9ae6288c8f60738
Xu Zhiyong <13980668829@163.com>
2022-05-26 10:50:59 +0800

    bug fix for forget prevEntity updating.

--- core/src/main/java/com/focus617/core/engine/ecs/mine/static/EntityHelper.kt
+++ core/src/main/java/com/focus617/core/engine/ecs/mine/static/EntityHelper.kt
@@ -9,17 +9,23 @@ fun Entity.setParent(parentEntity: Entity) {
     val parentRelationship = mapper[parentEntity]
     val childRelationship = mapper[this]
 
+    childRelationship.parent = parentEntity.id
+
     // Parent没有Child
     if (parentRelationship.childrenNumber == 0) {
         parentRelationship.first = this.id
         parentRelationship.last = this.id
+        parentRelationship.childrenNumber++
     }
     // Parent已经有Children
     else {
         // 将自己添加到Parent的表尾
         childRelationship.prev = parentRelationship.last
         parentRelationship.last = this.id
+        parentRelationship.childrenNumber++
+
+        // 同时修改last Entity的next
+        val prevEntity = Entity(childRelationship.prev)
+        mapper[prevEntity].next = this.id
     }
-    childRelationship.parent = parentEntity.id
-    parentRelationship.childrenNumber++
 }
